3a3011a4f2cf80342b6565857057688dceab91ad
oswee <dzintars.klavins@gmail.com>
2018-07-11 16:08:25 +0300

    Initial refactoring of Customer example

--- server.go
+++ server.go
@@ -1,39 +1,53 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
+	"strings"
 
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 
-	"github.com/oswee/proto/api/go"
-	"github.com/oswee/server/handlers"
+	pb "github.com/oswee/proto/customer/go"
 )
 
-// main starts a gRPC server and waits for connection
+const (
+	port = ":50051"
+)
+
+// server is used to implement customer.CustomerServer.
+type server struct {
+	savedCustomers []*pb.CustomerRequest
+}
+
+// CreateCustomer creates a new Customer
+func (s *server) CreateCustomer(ctx context.Context, in *pb.CustomerRequest) (*pb.CustomerResponse, error) {
+	s.savedCustomers = append(s.savedCustomers, in)
+	return &pb.CustomerResponse{Id: in.Id, Success: true}, nil
+}
+
+// GetCustomers returns all customers by given filter
+func (s *server) GetCustomers(filter *pb.CustomerFilter, stream pb.Customer_GetCustomersServer) error {
+	for _, customer := range s.savedCustomers {
+		if filter.Keyword != "" {
+			if !strings.Contains(customer.Name, filter.Keyword) {
+				continue
+			}
+		}
+		if err := stream.Send(customer); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func main() {
-	// create a listener on TCP port 7777
-	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", "localhost", 7777))
+	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	// create a server instance
-	s := handlers.Server{}
-	// Create the TLS credentials
-	creds, err := credentials.NewServerTLSFromFile("cert/server.crt", "cert/server.key")
-	if err != nil {
-		log.Fatalf("could not load TLS keys: %s", err)
-	}
-	// Create an array of gRPC options with the credentials
-	opts := []grpc.ServerOption{grpc.Creds(creds)}
-	// create a gRPC server object
-	grpcServer := grpc.NewServer(opts...)
-	// attach the Ping service to the server from API PB
-	api.RegisterPingServer(grpcServer, &s)
-	// start the server
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %s", err)
-	}
+	// Creates a new gRPC server
+	s := grpc.NewServer()
+	pb.RegisterCustomerServer(s, &server{})
+	s.Serve(lis)
 }
